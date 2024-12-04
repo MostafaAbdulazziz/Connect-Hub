f8b257c0cc335b1a3bf6d570762ef763a0258bd0
mostafazizo66 <mostafa.zizp@gmail.com>
2024-12-04 22:14:12 +0200

    adding GUI components & elements

---
+++ src/com/socialnetwork/connecthub/frontend/swing/components/JLabel.java
@@ -0,0 +1,15 @@
+package com.socialnetwork.connecthub.frontend.swing.components;
+
+import java.awt.Color;
+import java.awt.Font;
+
+@SuppressWarnings("serial")
+public class JLabel extends javax.swing.JLabel {
+	
+	public JLabel(String text, int textSize, Color color, int style) {
+		setFont(new Font("Segoe UI", style, textSize));
+		setText(text);
+		setForeground(color);
+	}
+
+}
