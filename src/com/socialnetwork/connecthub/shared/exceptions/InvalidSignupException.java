2fc5135910b66032b8548f1e32c95cc9765dd042
Malek Attia <malekalmansory05@gmail.com>
2024-12-05 01:54:10 +0200

    add initial exceptions

---
+++ src/com/socialnetwork/connecthub/shared/exceptions/InvalidSignupException.java
@@ -0,0 +1,7 @@
+package com.socialnetwork.connecthub.shared.exceptions;
+
+public class InvalidSignupException extends SocialNetworkException {
+    public InvalidSignupException(String message) {
+        super(message);
+    }
+}
