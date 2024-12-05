d5fa867535d7c75f27ca39f4090c0c6410934e85
Malek Attia <malekalmansory05@gmail.com>
2024-12-05 03:44:29 +0200

    using content DTOs in NewsFeedService

--- src/com/socialnetwork/connecthub/backend/interfaces/services/NewsFeedService.java
+++ src/com/socialnetwork/connecthub/backend/interfaces/services/NewsFeedService.java
@@ -1,11 +1,11 @@
 package com.socialnetwork.connecthub.backend.interfaces.services;
 
-import com.socialnetwork.connecthub.backend.model.Post;
+import com.socialnetwork.connecthub.shared.dto.ContentDTO;
 import com.socialnetwork.connecthub.shared.dto.UserDTO;
 import java.util.List;
 
 public interface NewsFeedService {
-    List<Post> getNewsFeed(String userId);
+    List<ContentDTO> getNewsFeed(String userId);
     List<UserDTO> getOnlineFriends(String userId);
     List<UserDTO> getFriendSuggestions(String userId);
 }
