2f2df5f6a67f7877082de779f7c455aa1bc7ba93
mostafazizo66 <mostafa.zizp@gmail.com>
2024-12-06 03:45:58 +0200

    myProfile and profile views finished1.7

--- src/com/socialnetwork/connecthub/frontend/swing/view/LoginView.java
+++ src/com/socialnetwork/connecthub/frontend/swing/view/LoginView.java
@@ -1,5 +1,7 @@
 package com.socialnetwork.connecthub.frontend.swing.view;
+
 import com.socialnetwork.connecthub.frontend.swing.components.JButton;
+
 public class LoginView extends View {
     JButton loginButton;
     JButton signUpButton;
