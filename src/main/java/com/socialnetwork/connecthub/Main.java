34ca8a97fc279edac21ff84a55afbb691c11c701
mostafazizo66 <mostafa.zizp@gmail.com>
2024-12-13 01:14:32 +0200

    creating and implementing MyGroupsView & adding some GrouoServices

--- src/main/java/com/socialnetwork/connecthub/Main.java
+++ src/main/java/com/socialnetwork/connecthub/Main.java
@@ -1,17 +1,18 @@
 package com.socialnetwork.connecthub;
 
-import com.socialnetwork.connecthub.backend.api.JavaSocialNetworkAPI;
 import com.socialnetwork.connecthub.backend.model.User;
 import com.socialnetwork.connecthub.backend.persistence.json.JsonUserRepository;
 import com.socialnetwork.connecthub.frontend.swing.navigationhandler.NavigationHandlerFactory;
-import com.socialnetwork.connecthub.shared.dto.LoginDTO;
 import com.socialnetwork.connecthub.shared.dto.UserDTO;
 
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        NavigationHandlerFactory.getNavigationHandler("final").goToLoginView();
+//        NavigationHandlerFactory.getNavigationHandler("final").goToLoginView();
+        List<User> user = JsonUserRepository.getInstance().findAll();
+        for (int i = 0; i < 2; i++) {
+            NavigationHandlerFactory.getNavigationHandler("final").goToNewsFeedView(new UserDTO(user.get(i)));
+        }
     }
 }
-
